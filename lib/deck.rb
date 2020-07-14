01ec66f966ef0c665369fc8728927667d0610314
KHong <albtrossfacefall@gmail.com>
2020-07-14 11:28:41 -0400

    begin adding deck

---
+++ lib/deck.rb
@@ -0,0 +1,16 @@
+class Deck
+  def initialize
+  end
+
+  def hello_world
+    return "hello world"
+  end
+
+  def draw_tarot(number_of_cards = 1)
+  end
+
+  def draw_playing_card(number_of_cards = 1)
+  end
+
+
+end
